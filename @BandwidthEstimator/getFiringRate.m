cfd723caed264980e64770726e6aa80450be11f0
= <alec.hoyland@gmail.com>
2018-08-29 11:08:05 -0400

    Documentation

--- @BandwidthEstimator/getFiringRate.m
+++ @BandwidthEstimator/getFiringRate.m
@@ -1,6 +1,6 @@
 function rate = getFiringRate(self, bandwidth)
 
-  % wraps the kconv function in a CMBHOME object
+  % wraps the kconv function in a BandwidthEstimator object
   filter = hanning(bandwidth) / sum(hanning(bandwidth));
   rate = self.kconv(self, filter);
 
