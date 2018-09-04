5203ddd23ec64fb511113f222c217e9e781fcd7c
= <alec.hoyland@gmail.com>
2018-09-04 12:20:31 -0400

    Remove extraneous code

--- @BandwidthEstimator/cvKernel.m
+++ @BandwidthEstimator/cvKernel.m
@@ -30,14 +30,7 @@ function [estimate, kmax, loglikelihoods, bandwidths, CI] = cvKernel(self, paral
   spikeTrain  = self.spikeTrain;
   Fs          = self.Fs;
   dt          = 1 / Fs;
-
-  % if kernel is a character vector, find the appropriate static method
-  % otherwise, kernel should be a function handle
-  if ischar(self.kernel)
-    kernel      = str2func(['BandwidthEstimator.' self.kernel]);
-  else
-    kernel      = self.kernel;
-  end
+  kernel      = best.kernel;
 
   if ~any(spikeTrain)
       estimate=zeros(1,length(spikeTrain));
