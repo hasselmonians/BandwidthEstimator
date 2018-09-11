62043aa3967f565b3ba33c3dda49bcc411da990f
= <alec.hoyland@gmail.com>
2018-09-11 10:32:03 -0400

    Remove useless function

--- @BandwidthEstimator/getFiringRate.m
+++
@@ -1,7 +0,0 @@
-function rate = getFiringRate(self, bandwidth)
-
-  % wraps the kconv function in a BandwidthEstimator object
-  filter = hanning(bandwidth) / sum(hanning(bandwidth));
-  rate = self.kconv(filter);
-
-end % function
