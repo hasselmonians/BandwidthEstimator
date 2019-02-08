e6ee3b81ff11239e1f14b54f4d257e9a7d7f23ca
Alec Hoyland <alec.hoyland@posteo.net>
2019-02-08 14:26:38 -0500

    Add comments and fix filename parsing

--- @BandwidthEstimator/batchFunction.m
+++ @BandwidthEstimator/batchFunction.m
@@ -1,10 +1,12 @@
 function batchFunction(index, location, outfile, test)
 
+  %% Preamble
+
   if nargin < 4
     test = false;
   end
 
-  % preamble
+  % if test is false, do not add to the matlab path
   if ~test
     addpath(genpath('/projectnb/hasselmogrp/hoyland/MLE-time-course/'))
     addpath(genpath('/projectnb/hasselmogrp/hoyland/RatCatcher/'))
@@ -14,27 +16,40 @@ function batchFunction(index, location, outfile, test)
     import CMBHOME.*
   end
 
-  % acquire the filename and cell number
-  fileID      = fopen([location filesep 'filenames.txt'], 'r');
-  filename    = cell2char(textscan(fileID, '%[^\n]', index));
-  fclose(fileID);
+  %% Read data
+
+  % load the entire filename file
+  % this is slow, but MATLAB has clunky textread options
+  filename    = lineRead([location filesep 'filenames.txt']);
+  % acquire only the character vector corresponding to the indexed filename
+  filename    = filename{index};
+  % acquire the cell number using similarly clunky indexing
   cellnum     = csvread([location filesep 'cellnums.csv'], index-1, 0, [index-1, 0, index-1, 1]);
 
-  % acquire data using function arguments
+  %% Load data
+
+  % load the root object from the specified raw data file
   load(filename);
   root.cel    = cellnum;
   root        = root.AppendKalmanVel;
   speed       = root.svel;
 
-  % generate the Bandwidth Estimator
+  %% Generate the Bandwidth Estimator
+
   best        = BandwidthEstimator(root);
-  best.parallel = true;
+  best.parallel = true; % see if this works on cluster ...
   best.range  = 3:2:(60*best.Fs);
   best.kernel = 'alpha';
+
   % perform bandwidth parameter estimate with MLE/CV
   [~, kmax, ~, ~, CI, kcorr, ~] = best.cvKernel(speed);
 
-  % save the data
-  csvwrite(outfile, [kmax CI kcorr]);
+  %% Save the data
+
+  if index == 1
+    csvwrite(outfile, [kmax, CI, kcorr]);
+  else
+    dlmwrite(outfile, [kmax CI kcorr], 'delimiter', ',', '-append');
+  end
 
 end % function
