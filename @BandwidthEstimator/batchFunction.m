a38824acceee40067b0842ddc9c37639b19eec23
Pranav Nambiar <pranavnambiar@dhcp-wifi-8021x-155-41-61-236.bu.edu>
2019-02-14 11:15:47 -0500

    documenting code

--- @BandwidthEstimator/batchFunction.m
+++ @BandwidthEstimator/batchFunction.m
@@ -18,18 +18,19 @@ function batchFunction(index, batchname, location, outfile, test)
 
   %% Read data
 
-  [filename, cellnum] = RatCatcher.read(location, batchname, index);
+  [filename, cellnum] = RatCatcher.read(location, batchname, index); %
 
   %% Load data
 
   % load the root object from the specified raw data file
-  load(filename);
-  root.cel    = cellnum;
+  % root object is the raw data from the experiment
+  load(filename); % load data on cluster indicated by filename and root object is loaded
+  root.cel    = cellnum; % determine cell number to look at
   root        = root.AppendKalmanVel;
-  speed       = root.svel;
+  speed       = root.svel; % speed is set by scaled velocity
 
   %% Generate the Bandwidth Estimator
-
+  % generate BandwidthEstimator object and determine properties within it
   best        = BandwidthEstimator(root);
   best.parallel = false;
   best.range  = 3:2:(60*best.Fs);
@@ -37,9 +38,9 @@ function batchFunction(index, batchname, location, outfile, test)
 
   % perform bandwidth parameter estimate with MLE/CV
   [~, kmax, ~, ~, CI, kcorr, ~] = best.cvKernel(speed);
-
+  % best bandwidth parameter estimate given the algorithm, best confidence interval of the bandwidth paramter estimate (computed using fisher info), bandwidth parameter if you tried to find bandwidth parameter for maximum speed score
   %% Save the data
 
-  csvwrite(outfile, [kmax, CI, kcorr]);
+  csvwrite(outfile, [kmax, CI, kcorr]); % write and save information
 
 end % function
