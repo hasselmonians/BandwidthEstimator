62e31b06107cbc2b40eb8a56a506aa873c9c7c2b
Alec Hoyland <alec.hoyland@posteo.net>
2019-12-10 15:35:24 -0500

    Create a new class for this analysis

---
+++ @ExGaussian/ExGaussian.m
@@ -0,0 +1,18 @@
+classdef ExGaussian
+
+properties
+
+end % properties
+
+methods
+
+end % methods
+
+methods (Static)
+
+  p = exgaussian(x, mu, sigma, lambda)
+  batchFunction(index, location, batchname, outfile, test)
+
+end % static methods
+
+end % classdef
