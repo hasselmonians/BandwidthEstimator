1589c2d1bb182813b86b5bf083ab1eadeac7b334
Alec Hoyland <ahoyland@bu.edu>
2020-02-06 14:35:14 -0500

    Create test directory

---
+++ test/test_parallel.m
@@ -0,0 +1,28 @@
+r = RatCatcher;
+
+r.expID         = 'Holger';
+r.remotepath    = '/projectnb/hasselmogrp/ahoyland/RatCatcher/cluster/';
+r.localpath     = '/mnt/hasselmogrp/ahoyland/RatCatcher/cluster/';
+r.protocol      = 'BandwidthEstimator';
+r.project       = 'hasselmogrp';
+r.verbose       = true;
+r.mode          = 'parallel';
+r.threading     = 'multi';
+
+return
+
+% batch files
+r = r.batchify;
+
+% NOTE: run the 'qsub' command on the cluster now (see output in MATLAB command prompt)
+
+return
+
+% NOTE: once the cluster finishes, run the following commands
+
+% gather files
+r = r.validate;
+dataTable = r.gather;
+dataTable = r.stitch(dataTable);
+
+return
